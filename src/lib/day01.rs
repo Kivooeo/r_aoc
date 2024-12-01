4f9f1154cf881a1e578f6a3bbbfb14fde119665f
Kivooeo <Kivooeo123@gmail.com>
2024-12-01 21:19:39 +0500

    day1 smol fix

--- src/lib/day01.rs
+++ src/lib/day01.rs
@@ -11,155 +11,96 @@ impl<'a> Day1<'a> {
         }
     }
     pub fn solve_test(&self) -> isize {
-        let y = self
-            .test
-            .split("\r\n")
-            .collect::<Vec<_>>()
-            .into_iter()
+        let mut xs = vec![];
+        let mut ys = vec![];
+        self.test
+            .lines()
             .map(|x| {
                 x.split("   ")
                     .map(|x| x.parse::<isize>().unwrap())
-                    .collect::<Vec<_>>()
+                    .collect::<Vec<isize>>()
             })
-            .collect::<Vec<Vec<_>>>();
-        let mut xs = vec![vec![], vec![]];
-        for i in &y {
-            xs[0].push(i[0]);
-        }
-        for i in &y {
-            xs[1].push(i[1]);
-        }
-        xs[0].sort();
-        xs[1].sort();
-        dbg!(&xs);
-        xs[0]
-            .clone()
-            .into_iter()
-            .zip(xs[1].clone())
-            .map(|x| (x.0 - x.1).abs())
+            .flatten()
+            .enumerate()
+            .for_each(|(i, x)| match i % 2 == 0 {
+                true => xs.push(x),
+                false => ys.push(x),
+            });
+        xs.sort();
+        ys.sort();
+        xs.into_iter()
+            .zip(ys.into_iter())
+            .map(|(x, y)| (x - y).abs())
             .sum()
     }
+
     pub fn solve_input(&self) -> isize {
-        let y = self
-            .input
-            .split("\r\n")
-            .collect::<Vec<_>>()
-            .into_iter()
+        let mut xs = vec![];
+        let mut ys = vec![];
+        self.input
+            .lines()
             .map(|x| {
                 x.split("   ")
                     .map(|x| x.parse::<isize>().unwrap())
-                    .collect::<Vec<_>>()
+                    .collect::<Vec<isize>>()
             })
-            .collect::<Vec<Vec<_>>>();
-        let mut xs = vec![vec![], vec![]];
-        for i in &y {
-            xs[0].push(i[0]);
-        }
-        for i in &y {
-            xs[1].push(i[1]);
-        }
-        xs[0].sort();
-        xs[1].sort();
-        dbg!(&xs);
-        xs[0]
-            .clone()
-            .into_iter()
-            .zip(xs[1].clone())
-            .map(|x| (x.0 - x.1).abs())
+            .flatten()
+            .enumerate()
+            .for_each(|(i, x)| match i % 2 == 0 {
+                true => xs.push(x),
+                false => ys.push(x),
+            });
+        xs.sort();
+        ys.sort();
+        xs.into_iter()
+            .zip(ys.into_iter())
+            .map(|(x, y)| (x - y).abs())
             .sum()
     }
 
     pub fn solve_input_part_two(&self) -> isize {
-        let y = self
-            .input
-            .split("\r\n")
-            .collect::<Vec<_>>()
-            .into_iter()
+        let mut xs = vec![];
+        let mut ys = vec![];
+        self.test
+            .lines()
             .map(|x| {
                 x.split("   ")
                     .map(|x| x.parse::<isize>().unwrap())
-                    .collect::<Vec<_>>()
+                    .collect::<Vec<isize>>()
             })
-            .collect::<Vec<Vec<_>>>();
-        let mut xs = vec![vec![], vec![]];
-        for i in &y {
-            xs[0].push(i[0]);
-        }
-        for i in &y {
-            xs[1].push(i[1]);
-        }
-        xs[0].sort();
-        xs[1].sort();
-        // dbg!(&xs);
-        xs[0]
-            .clone()
-            .into_iter()
-            .map(|x| {
-                xs[1]
-                    .clone()
-                    .into_iter()
-                    .filter(move |y| {
-                        // dbg!(&y);
-                        x == *y
-                    })
-                    .count() as isize
-                    * x
-            })
+            .flatten()
+            .enumerate()
+            .for_each(|(i, x)| match i % 2 == 0 {
+                true => xs.push(x),
+                false => ys.push(x),
+            });
+        xs.sort();
+        ys.sort();
+        xs.into_iter()
+            .map(|x| x * ys.clone().into_iter().filter(|&y| x == y).count() as isize)
             .sum()
     }
 
     pub fn solve_test_part_two(&self) -> isize {
-        let y = self
-            .test
-            .split("\r\n")
-            .collect::<Vec<_>>()
-            .into_iter()
+        let mut xs = vec![];
+        let mut ys = vec![];
+        self.input
+            .lines()
             .map(|x| {
                 x.split("   ")
                     .map(|x| x.parse::<isize>().unwrap())
-                    .collect::<Vec<_>>()
+                    .collect::<Vec<isize>>()
             })
-            .collect::<Vec<Vec<_>>>();
-        let mut xs = vec![vec![], vec![]];
-        for i in &y {
-            xs[0].push(i[0]);
-        }
-        for i in &y {
-            xs[1].push(i[1]);
-        }
-        xs[0].sort();
-        xs[1].sort();
-        // dbg!(&xs);
-        xs[0]
-            .clone()
-            .into_iter()
-            .map(|x| {
-                xs[1]
-                    .clone()
-                    .into_iter()
-                    .filter(move |y| {
-                        // dbg!(&y);
-                        x == *y
-                    })
-                    .count() as isize
-                    * x
-            })
+            .flatten()
+            .enumerate()
+            .for_each(|(i, x)| match i % 2 == 0 {
+                true => xs.push(x),
+                false => ys.push(x),
+            });
+        xs.sort();
+        ys.sort();
+        xs.into_iter()
+            .map(|x| x * ys.clone().into_iter().filter(|&y| x == y).count() as isize)
             .sum()
     }
-
-    // pub fn solve_test(&self) -> i32 {
-    //     let mut x = self.parse_test();
-    //     let len = x.len() / 2;
-    //     dbg!(&x);
-
-    //     let temp = x.split_at_mut(len);
-    //     let z = temp.1;
-    //     z.sort();
-    //     let x = temp.0;
-    //     x.sort();
-
-    //     dbg!(&x);
-    //     dbg!(&z);
-    //     z.into_iter().zip(x).map(|x| (*x.0 - *x.1).abs()).sum()
-    // }
 }
